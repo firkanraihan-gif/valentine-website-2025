51e8b932a1e3621175551ca424f04c4382bde357
firkanraihan-gif <firkan.raihan@gmail.com>
2026-02-14 11:19:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, peluk, peluk sambil cipok, cipokan, semuanya pokonya yeay , !",
+        message: "Now come get your gift, peluk, peluk sambil cipok, cipokan, semuanya pokonya yeay LOVE YOUU SAYANG, !",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
